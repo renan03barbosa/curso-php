7e59c5a18b93f0c02a621f3c1e349b3c4802fc71
php 15 <php.15@metralha.cec.ime.usp.br>
2017-02-06 22:43:15 -0200

    Fibonaci modificado!

--- logica/exibefibonacci.php
+++ logica/exibefibonacci.php
@@ -3,23 +3,27 @@
 echo "\n";
 
 $var1 = 0;
-$var2 = 1;
+$var2 = 0;
+$limite = 15;
 
-//echo "$var1, $var1, ";
-
-for ($a = 1; $a <= 15; $a++){
+for ($a = 1; $a <= $limite; $a++){
   if ($var1 == 0){
     echo "$var1, ";
   }
-  $aux = $var1 + $var2;
-  $var1 = $var2;
+  $aux = $var1 + 1;
   $var2 = $aux;
 
-  if ($a == 15){
+  else{
+    $aux = $var1 + $var2;
+    $var1 = $var2;
+    $var2 = $aux;
+
+  if ($a == $limite){
     echo "$aux.";
   }
-  else{
-  echo "$aux, ";
+    else{
+      echo "$aux, ";
+    }
   }
 }
 echo "\n\n";
